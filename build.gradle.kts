33d2c68da1ffd8def09e098a53c01414e9dea761
Anh T. Nguyen <106876168+aviant-dev@users.noreply.github.com>
2026-09-06 02:04:17 +0700

    chore: adjust compatibility targets and update README (#63)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 import org.jetbrains.intellij.platform.gradle.IntelliJPlatformType
 import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 
 val targetIde = providers.gradleProperty("platformIde").orElse("idea-community")
 
@@ -9,14 +10,27 @@ plugins {
     id("org.jetbrains.changelog")
 }
 
+// Ladybug runs on Java 21 and supplies Kotlin 1.9 APIs.
+kotlin {
+    jvmToolchain(21)
+    compilerOptions {
+        languageVersion.set(KotlinVersion.KOTLIN_1_9)
+        apiVersion.set(KotlinVersion.KOTLIN_1_9)
+    }
+}
+
+tasks.test {
+    systemProperty("idea.kotlin.plugin.use.k2", providers.gradleProperty("kotlinK2").orElse("false").get())
+}
+
 dependencies {
     testImplementation("junit:junit:4.13.2")
 
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
         when (targetIde.get()) {
-            "android-studio" -> androidStudio("2025.1.1.13")
-            "idea-community" -> intellijIdeaCommunity("2025.1.6")
+            "android-studio" -> androidStudio("2024.2.1.9")
+            "idea-community" -> intellijIdeaCommunity("2024.2.1")
             else -> error(
                 "Unsupported platformIde '${targetIde.get()}'. Use 'idea-community' or 'android-studio'."
             )
@@ -31,9 +45,17 @@ dependencies {
 }
 
 intellijPlatform {
+    pluginConfiguration {
+        ideaVersion {
+            sinceBuild.set("242.21829.142")
+            untilBuild.set("251.*")
+        }
+    }
     pluginVerification {
         ides {
-            ide(IntelliJPlatformType.IntellijIdeaCommunity, "2025.1.6")
+            ide(IntelliJPlatformType.IntellijIdeaCommunity, "2024.2.1")
+            ide(IntelliJPlatformType.AndroidStudio, "2024.2.1.9")
+            ide(IntelliJPlatformType.AndroidStudio, "2025.1.1.13")
         }
     }
 }
